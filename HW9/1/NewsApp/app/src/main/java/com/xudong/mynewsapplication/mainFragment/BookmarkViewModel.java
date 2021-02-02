1bd6fd7361c7eb9f0eab44033656c520109df2e1
zhugexiu <50414474+zhugexiu@users.noreply.github.com>
2021-02-02 14:50:35 +0800

    Delete HW9/1 directory

--- HW9/1/NewsApp/app/src/main/java/com/xudong/mynewsapplication/mainFragment/BookmarkViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.xudong.mynewsapplication.mainFragment;
-
-import androidx.lifecycle.ViewModel;
-
-public class BookmarkViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
